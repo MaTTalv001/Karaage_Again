5de7222478439e8904ed41fbefd285c257291863
Kanae-Nishina <karubona.ra247@gmail.com>
2024-02-14 18:00:43 +0900

    App.jsの変更を取り消し。レイアウトにホバーアニメーション追加

--- src/pages/game/GamePlay.jsx
+++ src/pages/game/GamePlay.jsx
@@ -18,16 +18,16 @@ export const GamePlay = () => {
         <div className='w-full h-full flex gap-3'>
           <div className='w-1/4 flex flex-col'>
             <h3 className='my-4'>配置オブジェクト</h3>
-            <div className='h-full bg-slate-500'>
+            <div className='h-full bg-white border-solid border-slate-400 hover:border-slate-950 transition-all border-2'>
             </div>
           </div>
           <div className='w-3/4 flex flex-col'>
             <div className='flex justify-between items-center mx-5'>
-              <button className='bg-blue-200 py-2 px-4 my-2'>Reset</button>
+              <button className='bg-blue-200 hover:bg-blue-400 text-slate-500 hover:text-slate-950 transition-all py-2 px-4 my-2'>Reset</button>
               <h3>ゲーム画面</h3>
-              <button className='bg-red-200 py-2 px-4 my-2'>▶</button>
+              <button className='text-slate-500 hover:text-slate-950 bg-red-200 hover:bg-red-400 transition-all py-2 px-4 my-2'>▶</button>
             </div>
-            <div className='h-full bg-slate-500'>
+            <div className='h-full bg-white border-solid border-slate-400 hover:border-slate-950 transition-all border-2'>
             </div>
           </div>
         </div>
