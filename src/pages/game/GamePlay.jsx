fa822529243c80bd5b717a91f9b99ae9cf8e415a
Kanae-Nishina <karubona.ra247@gmail.com>
2024-02-14 17:43:54 +0900

    大枠微調整

--- src/pages/game/GamePlay.jsx
+++ src/pages/game/GamePlay.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export const GamePlay = () => {
   return (
-    <div className='h-screen flex flex-col justify-center'>
-      <header className='flex justify-between items-center bg-slate-300 h-14'>
+    <div className='h-screen flex flex-col justify-center m-auto bg-green-200'>
+      {/* TODO : headerはここでは書かない */}
+      <header className='flex justify-between items-center bg-slate-200 h-14'>
         <h1 className='ml-5'>Pythagora maker</h1>
         <nav className='mr-5'>
           <ul className='flex gap-4'>
@@ -13,8 +14,8 @@ export const GamePlay = () => {
           </ul>
         </nav>
       </header>
-      <div className='w-full h-[calc(100vh-3.5rem*2)] bg-green-200'>
-        <div className='w-full h-full flex p-3 gap-3'>
+      <div className='h-[800px] w-[1200px] m-auto'>
+        <div className='w-full h-full flex gap-3'>
           <div className='w-1/4 flex flex-col'>
             <h3 className='my-4'>配置オブジェクト</h3>
             <div className='h-full bg-slate-500'>
@@ -22,7 +23,7 @@ export const GamePlay = () => {
           </div>
           <div className='w-3/4 flex flex-col'>
             <div className='flex justify-between items-center mx-5'>
-              <button className='bg-slate-200 py-2 px-4 my-2'>Reset</button>
+              <button className='bg-blue-200 py-2 px-4 my-2'>Reset</button>
               <h3>ゲーム画面</h3>
               <button className='bg-red-200 py-2 px-4 my-2'>▶</button>
             </div>
@@ -31,7 +32,8 @@ export const GamePlay = () => {
           </div>
         </div>
       </div>
-      <footer className='h-14 flex justify-end items-center mr-3'>
+      {/* TODO : footerはここでは書かない */}
+      <footer className='flex justify-end items-center mb-2 mr-10'>
         <ul className='flex gap-4'>
           <li>COPY RIGHT</li>
           <li>プライバシーポリシー</li>
