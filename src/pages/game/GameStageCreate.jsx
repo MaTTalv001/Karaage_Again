f02db435ebbbe7895cd91cb78862113ba67c4510
Yuta Suzuki <g.uc2180@gmail.com>
2024-02-19 23:45:08 +0900

    CSステージåステージ制作画面のCSS調整

--- src/pages/game/GameStageCreate.jsx
+++ src/pages/game/GameStageCreate.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 export const GameStageCreate = () => {
   return (
-    <div className="w-full h-full relative font-[DotGothic16] mt-14">
-      <h1 className="translate-x-10 translate-y-10 font-bold text-3xl ">Create</h1>
+    <div className="max-w-screen-xl max-h-screen-lg relative font-[DotGothic16] mt-14">
+      <Link to="/game/new">
+        <h1 className="font-bold text-4xl flex ">Create</h1>
+      </Link>
+      <Link to="/game/edit">
+        <h1 className="font-bold text-4xl flex ">Edit</h1>
+      </Link>
     </div>
   )
 }
