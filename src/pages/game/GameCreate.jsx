f2680d292088eca95de91642cc7d135f458378df
Yuta Suzuki <g.uc2180@gmail.com>
2024-02-20 00:00:17 +0900

    ステージ新規作成画面の追加と、ルーティング設定

--- src/pages/game/GameCreate.jsx
+++ src/pages/game/GameCreate.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+
 
 export const GameCreate = () => {
   return (
-    <div className="">
-      GameCreate
+    <div className="max-w-screen-xl max-h-screen-lg relative font-[DotGothic16] mt-14">
+      <Link to="/game/make">
+        <h1 className="font-bold text-4xl flex mb-7">Create</h1>
+      </Link>
+      <Link to="/game/:id/edit">
+        <h1 className="font-bold text-4xl flex ">Edit</h1>
+      </Link>
     </div>
   )
 }
--- src/pages/game/GameStageCreate.jsx
+++ src/pages/game/GameStageCreate.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
-
 
 export const GameStageCreate = () => {
   return (
-    <div className="max-w-screen-xl max-h-screen-lg relative font-[DotGothic16] mt-14">
-      <Link to="/game/new">
-        <h1 className="font-bold text-4xl flex ">Create</h1>
-      </Link>
-      <Link to="/game/edit">
-        <h1 className="font-bold text-4xl flex ">Edit</h1>
-      </Link>
+    <div className="">
+      GameStageCreate
     </div>
   )
 }
