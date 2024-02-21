00b08b4c9f1bff7514ada26adbed385aa20567df
MaTTalv001 <pomeranian1981@gmail.com>
2024-02-22 06:35:12 +0900

    相対パス変更漏れを修正

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./contexts/AuthContext";
-import { ProfileProvider } from "./contexts/ProfileContext";
+import { AuthProvider } from "contexts/AuthContext";
+import { ProfileProvider } from "contexts/ProfileContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
