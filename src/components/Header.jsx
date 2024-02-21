6c12df448dbe691fd07b78456d6a48dc3d6c7129
MaTTalv001 <pomeranian1981@gmail.com>
2024-02-21 20:19:50 +0900

    ログイン時にauth.usersに関連づけられたProfileデータ（name, role）を取得

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,47 @@
-import React from "react";
-import { RoutePath } from "../utils/RouteSetting";
-import { Link } from "react-router-dom";
-import supabase from "services/supabaseClient";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "contexts/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { RoutePath } from "../utils/RouteSetting";
+import supabase from "services/supabaseClient";
+import { useProfile } from "../contexts/ProfileContext";
 
 const Header = () => {
   const { user } = useAuth();
+  const { profile } = useProfile();
   const navigate = useNavigate();
+
   const logout = async (e) => {
     e.preventDefault();
     try {
       const { error: logoutError } = await supabase.auth.signOut();
+
       if (logoutError) {
         throw logoutError;
       }
       alert("ログアウトしました");
-      await navigate(RoutePath.home.path);
+      navigate(RoutePath.home.path);
     } catch {
-      alert("エラーが発生しました");
+      alert("ログアウト時にエラーが起きました");
     }
   };
+
   return (
     <div className="font-bold bg-gray-200 text-black p-2.5 flex justify-between">
-      <div className="mx-5">
+      <div className="mx-5 flex items-center">
         <Link
           to={RoutePath.home.path}
-          className="text-xl mr-5  text-yellow-300 text-black-outline font-[DotGothic16]"
+          className="text-xl mr-5 text-yellow-300 text-black-outline font-[DotGothic16]"
         >
           {RoutePath.home.name}
         </Link>
+        {profile?.role === "管理者" && (
+          <span className="bg-green-300  px-2 mr-5 font-bold rounded-md">
+            Admin
+          </span>
+        )}
       </div>
       <div className="mx-5">
         <Link to="#" className="text-black mr-2 font-[DotGothic16]">
-          About_Us...
+          About Us...
         </Link>
         {user ? (
           <button
--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from './contexts/AuthContext';
+import { AuthProvider } from "./contexts/AuthContext";
+import { ProfileProvider } from "./contexts/ProfileContext";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   //<React.StrictMode>
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <ProfileProvider>
+        <App />
+      </ProfileProvider>
     </AuthProvider>
   </BrowserRouter>
   //</React.StrictMode>
 );
-
