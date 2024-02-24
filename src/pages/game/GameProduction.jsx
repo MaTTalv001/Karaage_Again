bb41d71d4bb3100144ababf5abfb8c501b880654
Yuta Suzuki <g.uc2180@gmail.com>
2024-02-24 16:10:10 +0900

    ゲーム制作画面の見た目作りとアイコン環境構築

--- src/pages/game/GameProduction.jsx
+++ src/pages/game/GameProduction.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { RoutePath } from "utils/RouteSetting";
 import { Link } from 'react-router-dom';
+import { IconContext } from 'react-icons'
+import { FiEdit2 } from "react-icons/fi";
+import { FiTrash2 } from "react-icons/fi";
+import { FiPlay } from "react-icons/fi";
 
 
 
@@ -11,11 +15,35 @@ export const GameProduction = () => {
         <h1 className="font-bold text-4xl flex mb-7">{RoutePath.gameMake.name}</h1>
       </Link>
       <div>
-        <h1 className="font-bold text-4xl">Edit</h1>
-        <ul>
-          <li>
-            <h1 className="font-bold">Title:aaaaa</h1>   {/* textは関数化する*/} 
-            <h1 className="font-bold">Open Now</h1> {/*textはstateで関数化する*/} 
+        <h1 className="font-bold text-4xl items-center justify-center flex">Edit</h1>
+        <ul className="flex-col">
+          <li className="flex my-14 items-center justify-center">
+            <div className="bg-gray-300 flex">
+              <div className="flex-col">
+                <h1 className="font-bold text-2xl py-4 px-8">Title:aaaaa</h1>   {/* textは関数化する*/} 
+                <h1 className="font-bold text-2xl py-4 px-8">Open Now</h1> {/*textはstateで関数化する*/} 
+              </div>
+              <div className="flex-col">
+                  <div className="pt-4 px-8">
+                    <FiEdit2 size={40}/>
+                  </div>
+                  <div className="pt-4 px-8">
+                    <FiTrash2 size={40}/>
+                  </div>            
+              </div>
+              <div className="flex-col">
+                <h1 className="font-bold text-2xl py-12 px-8 bg-red-400"><FiPlay />Test</h1>   {/* textは関数化する*/} 
+              </div>
+              <div className="flex-col">
+                <h1 className="font-bold text-2xl py-12 px-8">画像</h1>   {/* textは関数化する*/}               
+              </div>
+            </div>
+          </li>
+          <li className="flex my-7 items-center justify-center">
+
+          </li>
+          <li className="flex my-7 items-center justify-center">
+
           </li>
         </ul>
       </div>
