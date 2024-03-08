92968913cddd79e62efa400bfeceada9d0f55562
Kanae-Nishina <karubona.ra247@gmail.com>
2024-03-08 15:27:16 +0900

    マジックナンバーを定数化、タイトル部分の透過オブジェクトの配置をなるべく安定化

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,64 +1,69 @@
 import "./HomePage.css"
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
 import { useAuth } from "contexts/AuthContext";
 import { RoutePath } from "utils/RouteSetting";
 import { Bodies, Composite, Engine, Render, Runner } from "matter-js";
 
 const HomePage = () => {
-  const navigate = useNavigate();
-  const { user, isLoading } = useAuth();
+  const { user } = useAuth();
 
   useEffect(() => {
     matterInitialize();
   }, []);
 
-  useEffect(() => {
-    if (!user && !isLoading) {
-      navigate(RoutePath.home.path);
-    }
-  }, [user, isLoading, navigate]);
-
   // MatterEngineがゲーム専用に作ってしまっておりこちらへの実用が難しかったため、matter.jsでやっています。
   const matterInitialize = () => {
+    // 画面の描画域サイズを取得
+    const SCREEN_WIDTH = document.body.clientWidth;
+    const SCREEN_HEIGHT = document.body.clientHeight;
     const parent = document.getElementById("Back-Object");
     const engine = Engine.create();
     const render = Render.create({
       element: parent,
       engine: engine,
       options: {
-        width: 2000,
-        height: 750,
+        width: SCREEN_WIDTH,
+        height: SCREEN_HEIGHT,
         wireframes: false,
         background: "transparent",
       },
     });
     Render.run(render);
 
+    const RENDER = { fillStyle: "transparent" }
     // 床部分。透過して見えなくしている。
-    const ground = Bodies.rectangle(2000 / 2, 740, 2000, 30, { isStatic: true, render: { fillStyle: "transparent" } });
+    const ground = Bodies.rectangle(2000 / 2, SCREEN_HEIGHT - 30, 2000, 30, { isStatic: true, render: RENDER });
     // タイトル部分の物理判定。透過して見えなくしている。
-    const box = Bodies.rectangle((2000 / 2) - 235, 300, 520, 265, { isStatic: true, render: { fillStyle: "transparent" } });
+    // TODO : スクリーンサイズから計算してタイトル部分の物理オブジェクトの配置を割り出しているが、これで本当に行けるかちょっと不安
+    const POSITION_Y_ADJUST = 55;
+    const CENTER_POSITION = { x: SCREEN_WIDTH / 2, y: SCREEN_HEIGHT / 2 - POSITION_Y_ADJUST }
+    const TITLE_BOX_SIZE = { width: 520, height: 265 }
+    const box = Bodies.rectangle(CENTER_POSITION.x, CENTER_POSITION.y, TITLE_BOX_SIZE.width, TITLE_BOX_SIZE.height, { isStatic: true, render: RENDER });
     const composite = Composite.create();
     // ランダムで物理オブジェクトを生成
+    const SPAWN_HEIGHT = 500;
+    const SPAWN_HEIGHT_RANGE = 20;
+    const SPAWN_ANGLE = 360;
+    const SPAWN_SIZE = 50;
+    const SPAWN_TYPE = 3;
     for (let i = 0; i < 50; i++) {
-      const type = Math.floor(Math.random() * 3);
-      const posX = Math.random() * 2000;
-      const posY = Math.random() * 520 - 500;
-      const rotate = Math.random() * 360;
-      const size = 50;
+      // ごちゃっと感を出すためにランダムで生成
+      const objectType = Math.floor(Math.random() * SPAWN_TYPE);
+      const posX = Math.random() * SCREEN_WIDTH;
+      const posY = Math.random() * (SPAWN_HEIGHT + SPAWN_HEIGHT_RANGE) - SPAWN_HEIGHT;
+      const rotate = Math.random() * SPAWN_ANGLE;
       let createObject;
-      switch (type) {
-        case 1: // ボール
-          createObject = Bodies.polygon(posX, posY, 3, size, { rotate });
+      switch (objectType) {
+        case 1: // 三角形
+          createObject = Bodies.polygon(posX, posY, 3, SPAWN_SIZE, { rotate });
           break
         case 2: // 方形
-          createObject = Bodies.rectangle(posX, posY, size, size, { rotate });
+          createObject = Bodies.rectangle(posX, posY, SPAWN_SIZE, SPAWN_SIZE, { rotate });
           break;
-        default:
+        default: // 円
           // 大きくなりすぎてバランス悪くなるので他のサイズから半分にしています
-          createObject = Bodies.circle(posX, posY, size / 2, { rotate });
+          createObject = Bodies.circle(posX, posY, SPAWN_SIZE / 2, { rotate });
           break;
       }
       Composite.add(composite, createObject);
@@ -70,23 +75,21 @@ const HomePage = () => {
   }
 
   return (
-    <>
-      <div className="w-full h-full relative">
-        <div id="Back-Object" className="w-full absolute top-0 left-0 overflow-hidden"></div>
-        <div className="relative font-[DotGothic16] flex flex-col items-center justify-center h-full pt-10 z-10">
-          <div className="pt-5 pb-5 pl-15 pr-15 rounded-3xl border-4 border-black text-center font-dotgothic16 text-shadow-black ">
-            <h1 className="text-8xl my-3 mx-10 text-yellow-300">Pythagora<br />maker</h1>
-          </div>
-          {/* tailwindで表現が難しそうだったので別途cssファイル作成してそちらに記述しています。 */}
-          <Link
-            to={user ? RoutePath.stageSelect.path : "#"}
-            className="game-start-button"
-          >
-            Game Start
-          </Link>
+    <div className="w-full h-full relative pt-14">
+      <div id="Back-Object" className="w-screen h-screen absolute top-0 left-0 overflow-hidden"></div>
+      <div className="relative font-[DotGothic16] flex flex-col items-center justify-center h-full pt-10 z-10">
+        <div className="pt-5 pb-5 pl-15 pr-15 rounded-3xl border-4 border-black text-center font-dotgothic16 text-shadow-black ">
+          <h1 className="text-8xl my-3 mx-10 text-yellow-300">Pythagora<br />maker</h1>
         </div>
+        {/* tailwindで表現が難しそうだったので別途cssファイル作成してそちらに記述しています。 */}
+        <Link
+          to={user ? RoutePath.stageSelect.path : RoutePath.login.path}
+          className="game-start-button"
+        >
+          Game Start
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 export default HomePage;
